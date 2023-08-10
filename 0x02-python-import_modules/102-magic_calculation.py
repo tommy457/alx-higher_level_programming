5d84f154acbb1abb32511980df504b1346cf77d4
tommy457 <boukhalfaml1011@gmail.com>
2023-08-10 12:31:17 -0700

    refactore

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -7,6 +7,4 @@ def magic_calculation(a, b):
         for i in range(4, 6):
             c = add(c, i)
             return c
-    else:
-        return (sub(a, b))
-    return
+    return (sub(a, b))
