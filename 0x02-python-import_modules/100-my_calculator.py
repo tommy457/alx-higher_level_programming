209b369be07fa3954b670f6ba928b038ced704b2
tommy457 <boukhalfaml1011@gmail.com>
2023-08-10 10:58:24 -0700

    pycodestayle bug fiex

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,9 +2,9 @@
 import sys
 from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
-    
+
     argc = len(sys.argv) - 1
-    
+
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>".format())
         sys.exit(1)
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     operator = sys.argv[2]
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    
+
     if operator == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == '-':
