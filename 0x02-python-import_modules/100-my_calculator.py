979806a436736f6a66fe349c416cb458e75605b1
tommy457 <boukhalfaml1011@gmail.com>
2023-08-10 10:56:40 -0700

    list index bug fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,13 +2,17 @@
 import sys
 from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
+    
     argc = len(sys.argv) - 1
-    operator = sys.argv[2]
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+    
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>".format())
         sys.exit(1)
+
+    operator = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    
     if operator == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == '-':
