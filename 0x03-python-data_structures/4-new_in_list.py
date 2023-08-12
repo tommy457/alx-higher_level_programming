4bff2520583408419c06e00d73eacfbc59de0066
tommy457 <boukhalfaml1011@gmail.com>
2023-08-12 01:33:51 -0700

    fix index bug

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     copy_list = my_list[:]
-    argc = len(my_list)
+    argc = len(my_list) - 1
 
     if idx >= 0 and idx <= argc:
         copy_list[idx] = element
