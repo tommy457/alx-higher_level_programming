993714386a42f12bacc86e30bd7b89873dcd6291
tommy457 <boukhalfaml1011@gmail.com>
2023-08-12 01:19:36 -0700

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    copy_list = my_list[:]
+    argc = len(my_list)
+
+    if idx >= 0 and idx <= argc:
+        copy_list[idx] = element
+    return copy_list
