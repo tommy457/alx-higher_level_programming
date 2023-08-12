f5896855fee27e26164fcdf402f1f597826e2d5d
tommy457 <boukhalfaml1011@gmail.com>
2023-08-12 01:56:55 -0700

    big fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,3 +7,5 @@ def print_matrix_integer(matrix=[[]]):
             for num in row[:-1]:
                 print("{:d}".format(num), end=" ")
             print("{:d}".format(row[-1]))
+        else:
+            print()
