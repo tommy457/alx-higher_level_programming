38adc56fba37b5e8c253f5dbf8c54dba2868171e
tommy457 <boukhalfaml1011@gmail.com>
2023-08-12 00:34:14 -0700

    fixed loop

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+    argc = len(my_list)
+    for i in range(argc):
+        print("{}".format(my_list[i]))
