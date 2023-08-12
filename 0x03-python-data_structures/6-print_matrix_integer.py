61d01b3be6ad9f939fcc326dead855dd05aeb192
tommy457 <boukhalfaml1011@gmail.com>
2023-08-12 01:54:06 -0700

    formating bug fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         if row:
             for num in row[:-1]:
-                print(num, end=" ")
-            print(row[-1])
+                print("{:d}".format(num), end=" ")
+            print("{:d}".format(row[-1]))
