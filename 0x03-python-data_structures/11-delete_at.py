c5557acda5990e1aa970e267e85b5fd805b62da0
tommy457 <boukhalfaml1011@gmail.com>
2023-08-12 05:39:13 -0700

    condition refactor

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,6 +3,6 @@
 def delete_at(my_list=[], idx=0):
     argc = len(my_list) - 1
 
-    if idx < argc and idx > 0 and argc > 0:
+    if idx <= argc and idx > 0 and argc >= 0:
         my_list.remove(my_list[idx])
     return my_list
