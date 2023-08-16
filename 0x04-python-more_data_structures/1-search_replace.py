daddb2fc081657172bab00d6b0839a2283dd3caa
tommy457 <boukhalfaml1011@gmail.com>
2023-08-16 09:51:24 -0700

    fixed bug return new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    for i in len(my_list):
-        if my_list[i] == search:
-            my_list[i] = replace
+    if my_list:
+        new_list = my_list.copy()
+        for i in range(len(my_list)):
+            if new_list[i] == search:
+                new_list[i] = replace
+        return new_list
