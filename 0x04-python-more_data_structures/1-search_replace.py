902f07507d8ea0e1f2a0f1c69c9e150532032f52
tommy457 <boukhalfaml1011@gmail.com>
2023-08-16 09:58:22 -0700

    remove empty list check

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    if my_list:
-        new_list = my_list.copy()
-        for i in range(len(my_list)):
-            if new_list[i] == search:
-                new_list[i] = replace
-        return new_list
+    new_list = my_list.copy()
+    for i in range(len(my_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
