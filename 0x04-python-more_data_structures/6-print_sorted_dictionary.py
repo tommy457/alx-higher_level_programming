51926008b48051b5416da56c570bcbbe1b580ff5
tommy457 <boukhalfaml1011@gmail.com>
2023-08-16 10:39:15 -0700

    formating bug fixed

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 
 def print_sorted_dictionary(a_dictionary):
     for key, value in sorted(a_dictionary.items()):
-        print("{:s} : {}".format(key, value))
+        print("{:s}: {}".format(key, value))
