2261cf5ee141438a2056417deb7b55e1e3b39dc6
tommy457 <boukhalfaml1011@gmail.com>
2023-08-28 09:24:39 -0700

    Indez error added + new line + typo

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,10 @@ def safe_print_list(my_list=[], x=0):
     num = 0
     try:
         for i in range(x):
-            print(miy_list[i])
-            num++
-    except:
+            print(my_list[i], end='')
+            num += 1
+    except IndexError:
+        print()
         return num
-
+    print()
     return num
