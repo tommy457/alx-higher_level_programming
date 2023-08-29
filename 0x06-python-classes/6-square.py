01d3ad2bc83f0fe1450133a65be871a361b05667
tommy457 <boukhalfaml1011@gmail.com>
2023-08-29 05:47:45 -0700

    error fix in ValueError

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -25,7 +25,7 @@ class Square:
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError("ze must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
