6649159d2a9ec2ffb6c4adb6fe84c523daaeba25
tommy457 <boukhalfaml1011@gmail.com>
2023-08-29 02:44:47 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Class square"""
+
+
+class Square:
+    """Define square"""
+
+    def __init__(self, size):
+        """initialization of a new square
+        args:
+            size (int): size of the square
+        """
+        self.__size = size
