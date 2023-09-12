9ae3b5b0a1f2384f0375fc498d91a7227af88843
tommy457 <boukhalfaml1011@gmail.com>
2023-09-12 12:15:17 -0700

    task numbers mix-up

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,8 +4,8 @@
 import sys
 import os.path
 import json
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if os.path.exists("add_item.json"):
     items = load_from_json_file("add_item.json")
