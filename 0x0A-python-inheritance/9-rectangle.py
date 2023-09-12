dccfb3064a0eceda046c42c977c4938edbed3b34
tommy457 <boukhalfaml1011@gmail.com>
2023-09-12 03:43:26 -0700

    styling bug fixed

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,7 +4,6 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-
 class Rectangle(BaseGeometry):
     """Subclass representing a rectangle inherits from BaseGeometry"""
     def __init__(self, width, height):
