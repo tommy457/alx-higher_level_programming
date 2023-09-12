b366ecae2ec8abb9f2e97f72101c71cdc1045ead
tommy457 <boukhalfaml1011@gmail.com>
2023-09-12 07:35:20 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for read_file method"""
+
+def read_file(filename=""):
+    """Method that reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, mode="r", encoding="utf-8") as MyFile:
+        print(MyFile.read())
