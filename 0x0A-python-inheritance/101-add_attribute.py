34453f4a2542fd3266bf1b6cbc124871ebbac654
tommy457 <boukhalfaml1011@gmail.com>
2023-09-12 06:25:01 -0700

    pycodestyle bug fixed

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,9 +1,9 @@
 #!usr/bin/python3
 """Module for add_attribute method"""
 
+
 def add_attribute(obj, name, value):
     """Method adds a new attribute to an object"""
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, name, value)
-
