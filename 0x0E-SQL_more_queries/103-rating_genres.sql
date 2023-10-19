e61c327e894f79cb81bc0d51549a34617a1fa5dc
tommy457 <boukhalfaml1011@gmail.com>
2023-10-19 01:17:32 -0700

    bug fix

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,4 +1,4 @@
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS "rating" FROM tv_shows
+SELECT name, SUM(rate) AS "rating" FROM tv_shows
 JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
