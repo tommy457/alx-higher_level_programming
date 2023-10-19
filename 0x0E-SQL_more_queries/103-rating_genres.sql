005a01c5f546f072b26fd484b48e412f9f47dd19
tommy457 <boukhalfaml1011@gmail.com>
2023-10-19 01:19:48 -0700

    added comment

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,4 +1,5 @@
-SELECT name, SUM(rate) AS "rating" FROM tv_shows
+-- Script that lists all genres in the database.
+SELECT name, SUM(rate) AS 'rating' FROM tv_shows
 JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
