7d44030189964b167cdd9867ae7eda0d42911b23
tommy457 <boukhalfaml1011@gmail.com>
2023-11-13 02:53:53 -0800

    formating error fixed

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myNum = parseInt(process.argv[2]);
 if (myNum) {
-  console.log('My number : %d', myNum);
+  console.log('My number: %d', myNum);
 } else {
   console.log('Not a number');
 }
