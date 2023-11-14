eb4fc6d85b772a39630be02260262b0d7e2f4634
tommy457 <boukhalfaml1011@gmail.com>
2023-11-14 11:41:59 -0800

    task 102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const fileA = fs.readFileSync(process.argv[2], 'utf-8');
+const fileB = fs.readFileSync(process.argv[3], 'utf-8');
+fs.writeFileSync(process.argv[4], fileA + fileB, 'utf-8');
