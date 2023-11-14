70fa0599329c896c1ca81a167a3a70d338e0ed3e
tommy457 <boukhalfaml1011@gmail.com>
2023-11-14 11:43:29 -0800

    added new line

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,4 +3,4 @@
 const fs = require('fs');
 const fileA = fs.readFileSync(process.argv[2], 'utf-8');
 const fileB = fs.readFileSync(process.argv[3], 'utf-8');
-fs.writeFileSync(process.argv[4], fileA + fileB, 'utf-8');
+fs.writeFileSync(process.argv[4], fileA + '\n' + fileB + '\n', 'utf-8');
