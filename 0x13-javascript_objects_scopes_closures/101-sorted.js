0d0e488a70601ebd3e2ef26b684762ba93abaad5
tommy457 <boukhalfaml1011@gmail.com>
2023-11-14 10:54:21 -0800

    styeling bug fixed

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,16 +3,15 @@ const dict1 = require('./101-data.js').dict;
 const dict2 = occurrences(dict1);
 console.log(dict2);
 
-function occurrences(dict) {
-  let dict2 = {};
+function occurrences (dict) {
+  const dict2 = {};
   for (const [key, value] of Object.entries(dict)) {
     if (!(value in dict2)) {
       dict2[value] = [];
       dict2[value].push(key);
-    }
-    else {
+    } else {
       dict2[value].push(key);
     }
   }
   return dict2;
-};
+}
