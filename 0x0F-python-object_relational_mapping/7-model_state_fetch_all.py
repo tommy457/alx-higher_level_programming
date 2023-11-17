be7628734c6906f0811ba6968a41eb5000c2866b
tommy457 <boukhalfaml1011@gmail.com>
2023-11-17 11:43:48 -0800

    task 7 re-factor

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,18 +5,15 @@
 from model_state import Base, State
 from sqlalchemy import create_engine, select
 import sys
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    stmt = select([
-        State.id, State.name
-        ]).order_by(State.id.asc())
-
-    connection = engine.connect()
-    results = connection.execute(stmt).fetchall()
-
-    for res in results:
-        print("{}: {}".format(res[0], res[1]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
