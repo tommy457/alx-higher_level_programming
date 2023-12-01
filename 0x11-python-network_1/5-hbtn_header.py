ee6ab7c0a678f89fb9218615fd2cf541e2ad0715
tommy457 <boukhalfaml1011@gmail.com>
2023-12-01 09:10:03 -0800

    make code not be executed when imported

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,8 +4,8 @@ from sys import argv
 """
 Script that fetches a URL and prints value of the X-Request-Id.
 """
+if __name__ == "__main__":
+    url = argv[1]
+    req = requests.get(url).headers
 
-url = argv[1]
-req = requests.get(url).headers
-
-print(req["X-Request-Id"])
+    print(req["X-Request-Id"])
