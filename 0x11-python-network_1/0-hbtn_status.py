401ccbbf679c03d0c8d59ce3f5b12435a385f55f
tommy457 <boukhalfaml1011@gmail.com>
2023-12-01 01:40:35 -0800

    pycode bug fixed + main==name

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,11 @@ Script that fetches an url and prints the body.
 """
 import urllib.request
 
+if __name__ == "__main__":
 
-url = "https://intranet.alxswe.com/status"
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    body = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-    print(body.format(type(html), html, html.decode('utf-8')))
+    url = "https://intranet.alxswe.com/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        body = "Body response:\n\t- type: {}"\
+            "\n\t- content: {}\n\t- utf8 content: {}"
+        print(body.format(type(html), html, html.decode('utf-8')))
