b78ef9a16e858456be935b2093095cf7718f81f5
tommy457 <boukhalfaml1011@gmail.com>
2023-12-01 13:14:02 -0800

    Only 17%

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest) of the repository
+first argument will be the repository name.
+The second argument will be the owner name.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    url = f"https://api.github.com/repos/{argv[1]}/{argv[2]}/commits"
+    headers = {"Accept": "application/vnd.github+json",
+               "X-GitHub-Api-Version": "2022-11-28"}
+    req = requests.get(url=url, headers=headers)
+    commits = req.json()[:10]
+
+    for idx in range(10):
+        sha = commits[idx].get("sha")
+        author_name = ((commits[idx].get("commit")).get("author")).get("name")
+        print("{}: {}".format(sha, author_name))
