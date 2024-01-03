f60dd10c0bf8deb2e4408f054cad5711fd902cc4
tommy457 <boukhalfaml1011@gmail.com>
2024-01-03 04:32:18 -0800

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
